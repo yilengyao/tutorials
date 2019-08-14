1c0e8632125518214ccce047db857209980224ee
yilengyao <yilengyao@gmail.com>
2019-08-14 12:10:01 -0400

    php data types

---
+++ php/www/data.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Datatype</title>
+</head>
+<body>
+    <?php
+        $phrase= "To be or not to be";
+        $age = 30;
+        $gpa = 30.3;
+        $isMale = false;
+    ?>
+</body>
+</html>
