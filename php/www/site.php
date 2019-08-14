cee4312c84a79309834585f4952448ebaa0a6589
yilengyao <yilengyao@gmail.com>
2019-08-14 11:58:20 -0400

    running php server, and first html page

---
+++ php/www/site.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $characterName = "John";
+        $characterAge = 35;
+        echo "There once was a man named $characterName <br>";
+        echo "He was $characterAge years old <br>";
+        $characterName = "Mike";
+        echo "He really liked the name $characterName <br>";
+        echo "He didn't like being $characterAge <br>";
+    ?>
+</body>
+</html>
