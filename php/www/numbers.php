24b7d2cbf4b9451d4c59cad47221dc4a76e89ea1
yilengyao <yilengyao@gmail.com>
2019-08-14 13:08:38 -0400

    php numbers

---
+++ php/www/numbers.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Numbers</title>
+</head>
+<body>
+    <?php
+        $num = 10;
+        $num += 25;
+        echo $num;
+        echo "<br>";
+        echo max(2,10);
+    ?>
+</body>
+</html>
