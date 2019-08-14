fbd9b77a3575bf4bd48a4ece4e00c558ead39d36
yilengyao <yilengyao@gmail.com>
2019-08-14 14:44:13 -0400

    php array

---
+++ php/www/array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Array</title>
+</head>
+<body>
+    
+    <?php
+        $friends = array("Kevin", "Karen", "Oscal", "Jim");
+
+        echo $friends;
+        echo "<br>";
+        echo $friends[1];
+        echo "<br>";
+        echo count($friends); // number of elements in array
+    ?>
+
+</body>
+</html>
