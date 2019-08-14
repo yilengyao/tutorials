6f66118a050d2c490dc64424064dd88232a9f271
yilengyao <yilengyao@gmail.com>
2019-08-14 14:08:09 -0400

    php user input

---
+++ php/www/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Input</title>
+</head>
+<body>
+    <?php ?>
+        <form action="site.php" method="get">
+            Name: <input type="text" name="username">
+            Age: <input type="number" name="age">
+            <input type="submit">
+        </form>
+        <br>
+        Your name is <?php echo $_GET["username"];?>
+        <br>
+        Your age is <?php echo $_GET["age"];?>
+</body>
+</html>
