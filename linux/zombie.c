ac60874ce757baeca913cfe5fac2b269a9013fc3
Yi leng Yao <yilengyao@gmail.com>
2019-09-09 20:05:39 -0700

    linux process

---
+++ linux/zombie.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main()
+{
+    pid_t child_pid;
+    /* Create child*/
+    child_pid = fork();
+    if (child_pid > 0) {
+        /* Parent process */
+        sleep(60);
+    } else {
+        /*Child process. Exit immediately. */
+        exit(0);
+    }
+    return 0;
+}
