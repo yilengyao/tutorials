67b6cbb2fb962057dec162f6243427d3149501fa
Yi leng Yao <yilengyao@gmail.com>
2019-12-02 08:37:24 -0800

    spring annotation initiate and distroy methods

--- spring/spring-demo-qualifiers/src/com/yi/springdemo/TennisCoach.java
+++ spring/spring-demo-qualifiers/src/com/yi/springdemo/TennisCoach.java
@@ -2,7 +2,11 @@ package com.yi.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class TennisCoach implements Coach {
@@ -13,6 +17,18 @@ public class TennisCoach implements Coach {
     // default constructor
     public TennisCoach() {
         System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+    // define my init method
+    @PostConstruct
+    public void doMyStartupStuff () {
+        System.out.println(">> TennisCoach: inside of doMyStartupSTuff()");
+    }
+
+    // define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
     }
 
     @Override
