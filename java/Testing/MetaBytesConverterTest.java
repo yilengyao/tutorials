16d332d9d4a10acd1656af0cd8724300b33ac321
YilengYao <yilengyao@gmail.com>
2020-10-16 23:07:02 -0400

    change test to @test

--- java/Testing/MetaBytesConverterTest.java
+++ java/Testing/MetaBytesConverterTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.BeforeEach;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class MetaBytesConverterTest {
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
@@ -11,20 +13,20 @@ public class MetaBytesConverterTest {
         System.setOut(new PrintStream(outputStreamCaptor));
     }
 
-    @org.junit.jupiter.api.Test
-    void test1() {
+    @Test
+    public void test1() {
         MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
         assertEquals("2500 KB = 2 MB and 452 KB", outputStreamCaptor.toString().trim());
     }
 
-    @org.junit.jupiter.api.Test
-    void test2() {
+    @Test
+    public void test2() {
         MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
         assertEquals("Invalid Value", outputStreamCaptor.toString().trim());
     }
 
-    @org.junit.jupiter.api.Test
-    void test3() {
+    @Test
+    public void test3() {
         MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
         assertEquals("5000 KB = 4 MB and 904 KB", outputStreamCaptor.toString().trim());
     }
