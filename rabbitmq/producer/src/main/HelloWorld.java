a032ac8e11d10114161880b7b20f43c8f10cc627
YilengYao <yilengyao@gmail.com>
2020-11-09 18:07:59 -0500

    rabbitmq management using docker compose

---
+++ rabbitmq/producer/src/main/HelloWorld.java
@@ -0,0 +1,9 @@
+package producer;
+
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
